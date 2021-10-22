72fc2b99cb2af02084d2cb9db38359f34332801a
chandrasamanta <samanta.chandrakanta@gmail.com>
2021-10-22 21:57:46 +0530

    first commit

---
+++ src/test/java/com/chandra/azspringbootjpademo/AzspringbootjpademoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chandra.azspringbootjpademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzspringbootjpademoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
